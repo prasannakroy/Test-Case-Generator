1f98433ba5849a4a07e3d3c426e850af04e79f91
prasannakroy <prasannakroy2000@gmail.com>
2021-04-22 09:57:29 +0530

    Update

--- CPP Reference/TestCaseGenerator.cpp
+++ CPP Reference/TestCaseGenerator.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
-#include "Primitive.h"
-#include "Tree.h"
-#include "Matrix.h"
-#include "LinearGenerator.h"
-#include "NonLinearGenerator.h"
 using namespace std;
 #define int long long
 #define double long double
 
 void solve() {
-	auto lst = generateDirectedGraphList(10, 15, 1, 10, 250);
-	for (auto &i: lst) cout << i[0] << " " << i[1] << " " << i[2] << "\n";
+	cout << 100 << "\n";
+	for (int i = 1; i <= 100; i++) cout << i << "\n";
 }
 
 signed main() {
--- references/http_demo.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-// create server object
-http.createServer((req, res) => {
-    // Write Response
-    res.write('Hello World');
-    res.end();
-}).listen(5000, () => console.log('Server running....'));
