6b8ea0a337d18dd5047a5ffb13e867b88f9fee26
prasannakroy <prasannakroy2000@gmail.com>
2021-04-22 10:50:44 +0530

    Update

---
+++ CPP_Reference/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n << "\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t(1);
+    // cin >> t;
+    while (t--) solve();
+}
