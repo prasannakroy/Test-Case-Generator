6eadf40304898510c1d209de863a62fa48405277
prasannakroy <prasannakroy2000@gmail.com>
2021-04-25 17:38:59 +0530

    Practice

--- server/index.js
+++ server/index.js
@@ -18,14 +18,14 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server started at ${PORT}`);
-	let pathToCpp = path.join(__dirname, '/../CPP_Reference');
-	exec(`g++ ${pathToCpp}/Main.cpp -o Main`, (err, stdout, stdin) => {
-		exec(`./Main < input.txt > output.txt`, (err, stdout, stdin) => {
-			if (err) {
-				console.log(err.message);
-				return;
-			}
-			console.log('Done');
-		});
-	});
+	// let pathToCpp = path.join(__dirname, '/../CPP_Reference');
+	// exec(`g++ ${pathToCpp}/Main.cpp -o Main`, (err, stdout, stdin) => {
+	// 	exec(`./Main < input.txt > output.txt`, (err, stdout, stdin) => {
+	// 		if (err) {
+	// 			console.log(err.message);
+	// 			return;
+	// 		}
+	// 		console.log('Done');
+	// 	});
+	// });
 });
